8c14b7ec913eb4d9e0e3236bf5bd227445262c94
cbukuba <cbukuba@student.s19.be>
2021-09-20 16:03:19 +0000

    Exercise01

---
+++ ex01/ft_print_params.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/20 15:56:02 by cbukuba           #+#    #+#             */
+/*   Updated: 2021/09/20 15:56:07 by cbukuba          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	c;
+
+	c = 0;
+	while (str[c] != '\0')
+	{
+		write(1, &str[c], 1);
+		c ++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+		i ++;
+	}
+	return (0);
+}
