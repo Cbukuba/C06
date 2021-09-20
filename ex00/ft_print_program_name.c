509d9355a96a202166c96f94742d5f1fa07eac91
cbukuba <cbukuba@student.s19.be>
2021-09-20 15:54:59 +0000

    Exercise00

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/20 15:44:04 by cbukuba           #+#    #+#             */
+/*   Updated: 2021/09/20 15:44:07 by cbukuba          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	c;
+
+	c = 0;
+	while (str[c] != '\0')
+	{
+		write(1, &str[c], 1);
+		c ++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	argc = 1;
+	ft_putstr(argv[0]);
+	return (0);
+}
